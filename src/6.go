de693fc373724598aa8ff2d9489d114943dc594e
= <=>
2014-04-14 08:35:27 +0800

    add error

--- src/6.go
+++ src/6.go
@@ -1,2 +1,28 @@
 package main
 
+import (
+	"time"
+	"fmt"
+)
+
+type MyError struct {
+	when time.Time
+	what string
+}
+
+func (e *MyError) Error() string{
+	return fmt.Sprintf("at %v %s", e.when, e.what)
+}
+
+func run() error {
+	return &MyError{
+		time.Now(),
+		"can't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
