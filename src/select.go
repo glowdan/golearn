bed16164a5600f5693038bc62d58b12e8cab761c
gao.yongzhi <gao.yongzhi@immomo.com>
2014-04-17 14:11:10 +0800

    select

---
+++ src/select.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c<-x:
+			x, y = y, y+x
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i:=0;i<10;i++ {
+			fmt.Println(<-c)
+			//time.Sleep(time.Second)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
