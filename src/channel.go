74ea0c0ab8745053835e3e26aaddb72925e05c50
= <=>
2014-04-17 08:20:02 +0800

    channel

--- src/channel.go
+++ src/channel.go
@@ -1,54 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
-func createCounter(start int) chan int {
-	next := make(chan int)
-	go func(i int) {
-		for {
-			next <-i
-			i++
-		}
-	}(start)
-	return next
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum
 }
 
-func main() {
-	counterA := createCounter(2)
-	counterB := createCounter(102)
-	for i:=0;i<5;i++ {
-		a:=<-counterA
-		fmt.Println(a, <-counterB)
-	}
-	channels := make([]chan bool, 6)
-	for i:= range channels {
-		channels[i] = make(chan bool)
-	}
-	go func() {
-		for {
-			channels[rand.Intn(6)] <- true
-		}
-	}()
-	for i:=0;i<36;i++ {
-		var x int
-		select {
-		case <- channels[0]:
-			x=1
-		case <- channels[1]:
-			x=2
-		case <- channels[2]:
-			x=3
-		case <- channels[3]:
-			x=4
-		case <- channels[4]:
-			x=5
-		case <- channels[5]:
-			x=6
-		}
-		fmt.Printf("%d\n", x)
-	}
-	fmt.Println()
+func  main () {
+	a := []int{ 1, 2, 4, 3, 5, 6}
+	c1 := make(chan int)
+	c2 := make(chan int)
+	go sum(a[:len(a)/2], c1)
+	go sum(a[len(a)/2:], c2)
+	x, y := <-c1, <-c2
+	fmt.Println(x, y, x+y)
 }
